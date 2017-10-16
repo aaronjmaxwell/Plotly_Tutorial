6e4162b75b8cfcd1325ccf0cf6628f4adbb0aafd
Aaron J. Maxwell, PhDI only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2017-10-16 13:24:12 -0400

    Figure renaming and some tweaks.

--- plotly_scatter.js
+++ plotly_scatter.js
@@ -1,12 +1,12 @@
-d3.json("simple_scatter.json", function(error, data) {
+d3.json("scatter_simple.json", function(error, data) {
     window.PLOTLYENV=window.PLOTLYENV || {};
     window.PLOTLYENV.BASE_URL="https://plot.ly";
     Plotly.newPlot("Simple_Scatter", [data], {"title": "A Simple Scatter Plot"}, {"showLink": false, "linkText": "Export to plot.ly"});
 })
 
-d3.json("yline.json", function(error, yline) {
-    d3.json("uline.json", function(error, uline) {
-        d3.json("vline.json", function(error, vline) {
+d3.json("scatter_yline.json", function(error, yline) {
+    d3.json("scatter_uline.json", function(error, uline) {
+        d3.json("scatter_vline.json", function(error, vline) {
             var data = [yline, uline, vline]
             window.PLOTLYENV=window.PLOTLYENV || {};
             window.PLOTLYENV.BASE_URL="https://plot.ly";
